59551d3a09f6edb7466a19bb7ae515f9f9d33200
Lasse Faurby <lasse_fk@hotmail.com>
2021-10-06 15:21:01 +0200

    test no work :(

--- Assignment4.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment4.Entities.Tests/TaskRepositoryTests.cs
@@ -6,6 +6,8 @@ using System.IO;
 using Assignment4.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Assignment4.Entities.Tests
 {
@@ -22,17 +24,18 @@ namespace Assignment4.Entities.Tests
         }
 
         [Fact]
-        public void All_Returns_All_Tasks(){
-            var col = new HashSet<TaskDTO>();
+        public void All_Returns_All_Tasks()
+        {
+            var col = new List<TaskDTO>();
             var taskRep = new TaskRepository();
-            
-            var addButton = new TaskDTO { Title = "Add button", Description = "Add a button to the page", State = State.New };
-            var removeButton = new TaskDTO { Title = "Remove button", Description = "Removes a button from the page", State = State.New };
-            var editButton = new TaskDTO { Title = "Edit a button", Description = "Edts a button on the page", State = State.New };
 
-            var optimizeAlgorithm = new TaskDTO { Title = "Optimize a algorithm", Description = "Optimizes a algorithm in the backend", State = State.New };
-            var updateDatabase = new TaskDTO { Title = "Update the database", Description = "Updates the database", State = State.New };
-            var refactorStructure = new TaskDTO { Title = "Refactor the structure", Description = "Refactors the structure", State = State.New };
+            var addButton = new TaskDTO {Id = 1, Title = "Add button", Description = "Add a button to the page", AssignedToId = 1, State = State.New, Tags = new ReadOnlyCollection<string>(new List<string> { "1" }) };
+            var removeButton = new TaskDTO {Id = 2, Title = "Remove button", Description = "Removes a button from the page", AssignedToId = 2, State = State.New, Tags = new ReadOnlyCollection<string>(new List<string> { "1" }) };
+            var editButton = new TaskDTO {Id = 3, Title = "Edit a button", Description = "Edts a button on the page", AssignedToId = 1, State = State.New, Tags = new ReadOnlyCollection<string>(new List<string> { "1" }) };
+
+            var optimizeAlgorithm = new TaskDTO {Id = 4, Title = "Optimize a algorithm", Description = "Optimizes a algorithm in the backend", AssignedToId = 2, State = State.New, Tags = new ReadOnlyCollection<string>(new List<string> { "2" }) };
+            var updateDatabase = new TaskDTO {Id = 5, Title = "Update the database", Description = "Updates the database", AssignedToId = 3, State = State.New, Tags = new ReadOnlyCollection<string>(new List<string> { "2" }) };
+            var refactorStructure = new TaskDTO {Id = 6, Title = "Refactor the structure", Description = "Refactors the structure", AssignedToId = 3, State = State.New, Tags = new ReadOnlyCollection<string>(new List<string> { "2" }) };
 
             col.Add(addButton);
             col.Add(removeButton);
@@ -41,8 +44,13 @@ namespace Assignment4.Entities.Tests
             col.Add(updateDatabase);
             col.Add(refactorStructure);
 
+            var expected = col.AsReadOnly();
+
             var result = taskRep.All();
-            Assert.Equal(col, result);
+
+            var first = result.First();
+            System.Console.WriteLine(first.Tags.First());
+            Assert.Equal(expected, result);
         }
     }
 }
--- Assignment4.Entities/TaskRepository.cs
+++ Assignment4.Entities/TaskRepository.cs
@@ -1,19 +1,58 @@
 using System.Collections.Generic;
+using System.IO;
 using Assignment4.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Assignment4;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Collections.Immutable;
 
 namespace Assignment4.Entities
 {
     public class TaskRepository : ITaskRepository
     {
-        
-        
-        public TaskRepository(){
-            
+
+        KanbanContext context;
+
+
+        public TaskRepository()
+        {
+            var connectionString = "Server=localhost;Database=Kanban-Board;User Id=sa;Password=34a6e100-d809-488a-9ce4-cfd2aeac387b";
+
+            var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>().UseSqlServer(connectionString);
+            context = new KanbanContext(optionsBuilder.Options);
         }
 
         public IReadOnlyCollection<TaskDTO> All()
         {
-            
+            // var taskList = context.Tasks.ToList();
+
+            // System.Console.WriteLine(taskList);
+
+            // var taskDTOList = new List<TaskDTO>();
+            // foreach (var t in taskList) {
+            //     var taskDTO = new TaskDTO{Id = t.ID, 
+            //     AssignedToId = t.AssignedTo.ID, 
+            //     Description = t.Description, 
+            //     State = t.State, 
+            //     Tags = (IReadOnlyCollection<string>)t.Tags, 
+            //     Title = t.Title};
+
+            //     taskDTOList.Add(taskDTO);
+            // }
+
+            var tasks = context.Tasks;
+            var list = tasks.Select(t => new TaskDTO {
+                Id = t.ID,
+                AssignedToId = t.AssignedTo.ID,
+                Description = t.Description,
+                State = t.State,
+                Tags = t.Tags.Select(tag => tag.ToString()).ToList().AsReadOnly(),
+                Title = t.Title
+            });
+
+            return list.ToList<TaskDTO>().AsReadOnly();
         }
 
         public int Create(TaskDTO task)
